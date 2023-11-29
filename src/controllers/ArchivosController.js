c6dd4bbf447f56ccc4073e0e649f9dc911acb33a
PedroUpits <143873815+PedroUpits@users.noreply.github.com>
2023-11-28 23:54:32 -0300

    Agrego subida de archivos

---
+++ src/controllers/ArchivosController.js
@@ -0,0 +1,25 @@
+const ArchivosController = {}
+
+const rutaPrincipal = __dirname + '/../../tmp/';
+
+ArchivosController.subirArchivo = (req, res) => {
+    try {
+    const archivo = req.files.miArchivo;
+    const rutaGuardar = rutaPrincipal + archivo.name;
+
+    return archivo.mv(rutaGuardar, function(err) {
+       if (err) {
+         return res.status(500).json({error: err});
+    } else {
+        return res.json({mensaje: 'El archivo se subio correctamente'});
+    }
+
+ });
+    } catch (err) {
+        return res.status(500).json({error: 'Ocurrio un error al intentar subir el archivo'});
+}
+}
+
+
+
+module.exports = ArchivosController;
